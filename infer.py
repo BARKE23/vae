f33d317ba0488c5d104eebf4a405efcd822c91f5
hlh <2983848307@qq.com>
2023-03-26 14:56:48 +0800

    youhua tijiao

--- infer.py
+++ infer.py
@@ -3,6 +3,9 @@ import yaml
 import argparse
 import numpy as np
 from pathlib import Path
+
+from torch.nn.functional import interpolate
+
 from models import *
 from experiment import VAEXperiment
 import torch.backends.cudnn as cudnn
@@ -52,41 +55,33 @@ autoencoder = VAEXperiment.load_from_checkpoint(checkpoint, vae_model=model,
 encoder = autoencoder.model.encoder
 encoder.eval()
 
-# unic = "5B66"
-#unic = "672C"
-#unic = "4FAF"#hou
-#unic = "5349"#hui
-# unic = "6728"
 #unic = "4E00"#一
 #unic = "4E8C"#二
 #unic = "4E09"#三
 # embed 1 fake pre-processed images!
 # img = default_loader("/home/ubuntu/code/ttf_vae/all_pngs/uni6728_simfang.png") # mu
-#img = default_loader("/root/autodl-tmp/hlhdata/ttftopng/simfang_png/uni4E00_simfang.png") # yi
-# img1 = default_loader("/home/ubuntu/code/ttf_vae/all_pngs/uni672C_simfang.png") # ben
-# img1 = default_loader("/home/ubuntu/code/ttf_vae/all_pngs/uni6728_STHUPO.png") # mu
-# img1 = default_loader("/home/ubuntu/code/ttf_vae/all_pngs/uni6728_STXINGKA.png") # mu
-img = default_loader("/root/autodl-tmp/hlhdata/ttftopng/uni4E00_middle.png")
-img1 = default_loader("/root/autodl-tmp/hlhdata/ttftopng/STXINGKA_png/uni4E00_STXINGKA.png") # yi
+img = default_loader("/root/autodl-tmp/hlhdata/ttftopng/simfang_png/uni4E00_simfang.png") # yi
+img1 = default_loader("/root/autodl-tmp/hlhdata/ttftopng/uni4E00_middle.png")
+#img1 = default_loader("/root/autodl-tmp/hlhdata/ttftopng/STXINGKA_png/uni4E00_STXINGKA.png") # yi
 
 img_transforms = transforms.Compose([
                                               transforms.CenterCrop(148),
                                               transforms.Resize(config['data_params']['patch_size']),
                                               transforms.Grayscale(num_output_channels=1),
                                               transforms.ToTensor(),]) #(H*W*C)[0,255] -> (C*H*W)[0.0,1.0]
-img = img_transforms(img)
-img1 = img_transforms(img1)
-imgs = torch.stack((img, img1))
+img = img_transforms(img)#torch.Size([1, 64, 64])
+img1 = img_transforms(img1)#torch.Size([1, 64, 64])
+imgs = torch.stack((img, img1))#torch.Size([2,1, 64, 64])
 
-#print(img.shape)#torch.Size([1, 64, 64])
-#print(img1.shape)#torch.Size([1, 64, 64])
-#print(imgs.shape)#torch.Size([2, 1, 64, 64])
+#白色1.0
+same_elements = (img == 1.0) & (img1 == 1.0)
+same_indices = torch.nonzero(same_elements)
 
 fake_image_batch = imgs
 # fake_image_batch = torch.rand(1, 1, 64, 64)
 [mu, log_var] = autoencoder.model.encode(fake_image_batch)
 
-#print(mu,"\n⚡⚡⚡\n",log_var)
+#print(mu.size(),log_var.size());#torch.Size([2, 128])
 
 embeddings = autoencoder.model.reparameterize(mu, log_var)
 print("⚡" * 20, "\nPredictions (32 image embeddings):\n", embeddings.shape, "\n", "⚡" * 20)#[2,128]
@@ -112,8 +107,26 @@ img1 = img1.clone().detach().reshape(1, 1, 64, 64)
 decoded_imgs = autoencoder.model.decode(embeddings)
 decoded_imgs = torch.cat((img, decoded_imgs, img1))
 
+'''folder_path = "yi1"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+for i in range(decoded_imgs.shape[0]):
+    for index in same_indices:
+        decoded_imgs[i][tuple(index)] = 1.0
+    file_name = f"yi_test_{i}.png"
+    file_path = os.path.join(folder_path, file_name)
+    vutils.save_image(decoded_imgs[i], file_path, normalize=True)'''
+
+for i in range(decoded_imgs.shape[0]):
+    for index in same_indices:
+        decoded_imgs[i][tuple(index)] = 1.0
+
+for j in range(decoded_imgs.shape[0]):
+    decoded_imgs[i][(decoded_imgs[i] > 0) & (decoded_imgs[i] < 1)]=0
+
 vutils.save_image(decoded_imgs.data,
-                  "yi_test1.png",
+                  "yimiddle.png",
                   normalize=True,
                   nrow=12)
 
--- run.py
+++ run.py
@@ -19,7 +19,7 @@ parser.add_argument('--config',  '-c',
                     dest="filename",
                     metavar='FILE',
                     help =  'path to the config file',
-                    default='configs/cvae.yaml')
+                    default='configs/bbvae.yaml')
 
 args = parser.parse_args()
 with open(args.filename, 'r') as file:
